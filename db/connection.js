9851e700148f2222724aa4afe8ac1663078407a0
Admin <admin@Admins-MacBook-Pro.local>
2019-01-18 10:01:40 +0000

    remove duplciate statement on connection

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,8 @@
 const ENV = process.env.NODE_ENV || 'development';
 // if not set, default to development
 // required in config
-const dbConfig = require('../knexfile.js')[ENV];
 
-const config =	ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
+const config =
+	ENV === 'production' ? {client: 'pg', connection: process.env.DATABASE_URL} : require('../knexfile')[ENV];
 
 module.exports = require('knex')(config);
-
-const knex = require('knex');
-
-const connection = knex(dbConfig);
-
-module.exports = connection;
