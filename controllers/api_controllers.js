9d52c037a62f61ed01d85ab2fd23bc211f39545b
Admin <admin@Admins-MacBook-Pro.local>
2019-01-18 12:09:18 +0000

    controller sorted

--- controllers/api_controllers.js
+++ controllers/api_controllers.js
@@ -1,6 +1,6 @@
 const endPoints = require('../db/data/endPoints');
 
 function getAllEndpoints(req, res, next) {
-  res.status(200).send({ endPoints }).catch(next);
+	res.status(200).send({endPoints});
 }
 module.exports = getAllEndpoints;
